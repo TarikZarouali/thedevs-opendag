f3d3dc3cfd66e2138a50b6720a2f371a1959b3cd
zylah11 <79306351+zylah11@users.noreply.github.com>
2022-09-16 16:13:50 +0200

    Added the required feature, this is not the end product this is just a sketch to improve on in further sprints

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+    <ul>
+        <li><a href="register.php">Registreer</a></li>
+        <li><a href="login.php">Log in</a></li>
+
+    </ul>
+
+    </body>
+</html>
