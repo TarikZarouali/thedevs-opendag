3a7e7a9de7bfa37061617fbc7acfa7b1dde860f4
allait330481 <330481@student.mboutrecht.nl>
2022-09-19 12:00:23 +0200

    Added navbar and landingpage to complete aceptable criteria
    This is a start of files need to be refined with later sprints

---
+++ Landingpage.php
@@ -0,0 +1,102 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>SkateRoom</title>
+</head>
+
+<body>
+
+    <header>
+
+        <a href="#" class="logo">MboUtrecht</a>
+
+        <nav class="navbar">
+            <a class="active" href="Landingpage.php">home</a>
+
+        </nav>
+
+
+    </header>
+
+
+    <div id="section-landing">
+
+        <div class="wrapper-landing">
+            <h1>MBO Utrecht</h1>
+        </div>
+    </div>
+
+
+    <div class="sec-home">
+
+        <section id="projects">
+            <div class="projects container">
+                <div class="projects-header">
+                    <h1 class="section-title">Onze aanbevelingen</h1>
+                </div>
+                <div class="all-projects">
+                    <div class="project-item">
+                        <div class="project-info">
+                            <h1>De nieuwe School gebouw </h1>
+                            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit..</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus esse, omnis dolore nemo sequi quisquam, deleniti modi consequuntur sunt, porro alias eos a pariatur maiores voluptas repellendus saepe eaque odio?</p>
+                        </div>
+                        <div class="project-img">
+                            <img src="https://www.opendag-mboutrecht.nl/sites/all/themes/mbo/ui/icon/brand/brand.jpg" alt="img">
+                        </div>
+                    </div>
+                    <div class="project-item">
+                        <div class="project-info">
+                            <h1>DEVELOPER</h1>
+                            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere laudantium in atque sapiente assumenda maxime aperiam, at eum voluptatibus provident consequuntur reiciendis impedit a, nemo vero autem doloremque. Illum, nesciunt?</p>
+                        </div>
+                        <div class="project-img">
+                            <img src="https://www.mboutrecht.nl/wp-content/uploads/2020/10/MBO-Utrecht-2020-algemeen-52-768x630.jpg" alt="img">
+                        </div>
+                    </div>
+                    <div class="project-item">
+                        <div class="project-info">
+                            <h1>Software</h1>
+                            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, amet quia error aut ipsam mollitia. Odio fugiat qui atque? Eveniet pariatur enim magnam omnis veniam porro reiciendis quo, aut repudiandae.</p>
+                        </div>
+                        <div class="project-img">
+                            <img src="https://media-exp1.licdn.com/dms/image/C561BAQFjvf6Db9m95w/company-background_10000/0/1639658294813?e=2147483647&v=beta&t=1Z5dobs4ScGUmzsV7mFrK1uWmGtimH4pHBNu_5sG3Us" alt="img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </section>
+    </div>
+
+    <footer>
+        <div class="footer-content">
+            <h3> © 2021 MBO Utrecht :</h3>
+            <p>MboUtrecht.nl</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy; <a href="#">MBO Utrecht</a> </p>
+            <div class="footer-menu">
+                <ul class="f-menu">
+
+
+                </ul>
+            </div>
+        </div>
+
+    </footer>
