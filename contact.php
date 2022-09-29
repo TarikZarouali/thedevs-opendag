dca74a303377dca0ad6874b94dbe3076e3cefae2
allait330481 <330481@student.mboutrecht.nl>
2022-09-29 10:05:02 +0200

    bugs fix

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,59 +8,61 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
+
 <body>
-<header>
+    <header>
         <a href="index.php" class="logo">MboUtrecht</a>
 
-     <nav>
-         <ul>
-             <li><a href="index.php">home</a></li>
-             <li><a href="course.html">courses</a></li>
-             <li><a href="">informatie</a>
-         <ul>
-             <li><a href="about.php">about</a></li>
-             <li><a href="contact.php">contact</a></li>
-        </ul>
-             <li><a href="">registreren?</a>
-         <ul>
-             <li><a href="register.php">registreer</a></li>
-             <li><a href="login.php">Login</a></li>
-        </ul>
-    </ul>
-     </nav>
- </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">home</a></li>
+                <li><a href="course.html">courses</a></li>
+                <li><a href="">informatie</a>
+                    <ul>
+                        <li><a href="about.php">about</a></li>
+                        <li><a href="contact.php">contact</a></li>
+                    </ul>
+                <li><a href="">registreren?</a>
+                    <ul>
+                        <li><a href="register.php">registreer</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+            </ul>
+        </nav>
+    </header>
 
 
-<form action="contact.php" method="post">
-    <div class="elem-group">
-        <label for="name">Your Name</label>
-        <input type="text" id="name" name="visitor_name" placeholder="Abdullatif Ahmad" pattern=[A-Z\sa-z]{3,20} required>
-    </div>
-    <div class="elem-group">
-        <label for="email">Your E-mail</label>
-        <input type="email" id="email" name="visitor_email" placeholder="Abdullatif.Ahmad@gmail.com" required>
-    </div>
-    <div class="elem-group">
-        <label for="department-selection">Choose Concerned Department</label>
-        <select id="department-selection" name="concerned_department" required>
-            <option value="">Select a Department</option>
-            <option value="billing">Billing</option>
-            <option value="marketing">Marketing</option>
-            <option value="technical support">Technical Support</option>
-        </select>
-    </div>
-    <div class="elem-group">
-        <label for="title">Reason For Contacting Us</label>
-        <input type="text" id="title" name="email_title" required placeholder="Unable to Reset my Password" pattern=[A-Za-z0-9\s]{8,60}>
-    </div>
-    <div class="elem-group">
-        <label for="message">Write your message</label>
-        <textarea id="message" name="visitor_message" placeholder="Say whatever you want." required></textarea>
-    </div>
-    <button type="submit">Send Message</button>
-</form>
-<style>
-    <?php include 'CSS/main.css'; ?>
-</style>
+    <form action="contact.php" method="post" class="contact_form">
+        <div class="elem-group">
+            <label for="name">Your Name</label>
+            <input type="text" id="name" name="visitor_name" placeholder="Abdullatif Ahmad" pattern=[A-Z\sa-z]{3,20} required>
+        </div>
+        <div class="elem-group">
+            <label for="email">Your E-mail</label>
+            <input type="email" id="email" name="visitor_email" placeholder="Abdullatif.Ahmad@gmail.com" required>
+        </div>
+        <div class="elem-group">
+            <label for="department-selection">Choose Concerned Department</label>
+            <select id="department-selection" name="concerned_department" required>
+                <option value="">Select a Department</option>
+                <option value="billing">Billing</option>
+                <option value="marketing">Marketing</option>
+                <option value="technical support">Technical Support</option>
+            </select>
+        </div>
+        <div class="elem-group">
+            <label for="title">Reason For Contacting Us</label>
+            <input type="text" id="title" name="email_title" required placeholder="Unable to Reset my Password" pattern=[A-Za-z0-9\s]{8,60}>
+        </div>
+        <div class="elem-group">
+            <label for="message">Write your message</label>
+            <textarea id="message" name="visitor_message" placeholder="Say whatever you want." required></textarea>
+        </div>
+        <button type="submit">Send Message</button>
+    </form>
+    <style>
+        <?php include 'CSS/main.css'; ?>
+    </style>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,66 +8,68 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
+
 <body>
     <header>
         <a href="index.php" class="logo">MboUtrecht</a>
 
-     <nav>
-         <ul>
-             <li><a href="index.php">home</a></li>
-             <li><a href="course.html">courses</a></li>
-             <li><a href="">informatie</a>
-         <ul>
-             <li><a href="about.php">about</a></li>
-             <li><a href="contact.php">contact</a></li>
-        </ul>
-             <li><a href="">registreren?</a>
-         <ul>
-             <li><a href="register.php">registreer</a></li>
-             <li><a href="login.php">Login</a></li>
-        </ul>
-    </ul>
-     </nav>
- </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">home</a></li>
+                <li><a href="course.html">courses</a></li>
+                <li><a href="">informatie</a>
+                    <ul>
+                        <li><a href="about.php">about</a></li>
+                        <li><a href="contact.php">contact</a></li>
+                    </ul>
+                <li><a href="">registreren?</a>
+                    <ul>
+                        <li><a href="register.php">registreer</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+            </ul>
+        </nav>
+    </header>
 
 </body>
+
 </html>
 
 <?php
 // Include config file
 require_once "db/config_db.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Validate username
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
         $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
+    } else {
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = :username";
-        
-        if($stmt = $pdo->prepare($sql)){
+
+        if ($stmt = $pdo->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-            
+
             // Set parameters
             $param_username = trim($_POST["username"]);
-            
+
             // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
+            if ($stmt->execute()) {
+                if ($stmt->rowCount() == 1) {
                     $username_err = "This username is already taken.";
-                } else{
+                } else {
                     $username = trim($_POST["username"]);
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -74,46 +77,46 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             unset($stmt);
         }
     }
-    
+
     // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
         $password_err = "Password must have atleast 6 characters.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password.";
+    } else {
         $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
+        if (empty($password_err) && ($password != $confirm_password)) {
             $confirm_password_err = "Password did not match.";
         }
     }
-    
+
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+
         // Prepare an insert statement
         $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
-         
-        if($stmt = $pdo->prepare($sql)){
+
+        if ($stmt = $pdo->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
-            
+
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
+
             // Attempt to execute the prepared statement
-            if($stmt->execute()){
+            if ($stmt->execute()) {
                 // Redirect to login page
                 header("location: login.php");
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -121,20 +124,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             unset($stmt);
         }
     }
-    
+
     // Close connection
     unset($pdo);
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Registreer</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body class="form">
     <div class="wrapper-credentials">
         <h2>Registreer</h2>
@@ -144,7 +149,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Gebruikersnaam</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group">
                 <label>Wachtwoord</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
@@ -155,12 +160,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div class="form-group"> 
+            <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Registreer">
                 <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
             <p>Heeft u al geregistreerd? <a href="login.php">Log hier in</a>.</p>
         </form>
-    </div>    
+    </div>
 </body>
+
 </html>
