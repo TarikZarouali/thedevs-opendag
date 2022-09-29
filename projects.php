09b65825e2f0dbfdafd9bc9b0b2fabb7fc35e440
Blackcobra660 <332496@student.mboutrecht.nl>
2022-09-29 09:33:45 +0200

    fixed: fixed a bug with the registration

--- projects.php
+++ projects.php
@@ -1,90 +1,85 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>Projects</title>
-  </head>
-  <body>
+</head>
 
-  <header>
+<body>
+
+    <header>
         <a href="index.php" class="logo">MboUtrecht</a>
 
-     <nav>
-         <ul>
-             <li><a href="index.php">home</a></li>
-             <li><a href="course.html">courses</a></li>
-             <li><a href="">informatie</a>
-         <ul>
-             <li><a href="about.php">about</a></li>
-             <li><a href="contact.php">contact</a></li>
-        </ul>
-             <li><a href="">registreren?</a>
-         <ul>
-             <li><a href="register.php">registreer</a></li>
-             <li><a href="login.php">Login</a></li>
-        </ul>
-    </ul>
-     </nav>
- </header>
-<section>
- 
-    <div id="projectcontainer-header">
-    <div class="projectheader">
-        <h1 class="projecttext1">School mapje</h1>
-        <div class="wrapper">
-            <div class="typing-demo">
-        <p class="projecttext2">Hier onder zijn mijn alle projecten.</p>
+        <nav>
+            <ul>
+                <li><a href="index.php">home</a></li>
+                <li><a href="course.html">courses</a></li>
+                <li><a href="">informatie</a>
+                    <ul>
+                        <li><a href="about.php">about</a></li>
+                        <li><a href="contact.php">contact</a></li>
+                    </ul>
+                <li><a href="">registreren?</a>
+                    <ul>
+                        <li><a href="register.php">registreer</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+            </ul>
+        </nav>
+    </header>
+    <section>
+
+        <div id="projectcontainer-header">
+            <div class="projectheader">
+                <h1 class="projecttext1">School mapje</h1>
+                <div class="wrapper">
+                    <div class="typing-demo">
+                        <p class="projecttext2">Dit ga je leren maken</p>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
 
 
-      <div id="projectcontainer-box">
-    <div class="projectcontainer mt-3">
-        <div class="projectrow">
+        <div id="projectcontainer-box">
+            <div class="projectcontainer mt-3">
+                <div class="projectrow">
 
-            <div class="col-4">
-                <div class="projectcard">
-                    <img src="img/img1.PNG" class="projectcard-img-top" alt="...">
-                    <div class="projectcard-body">
-                        <h5 class="projectcard-title">De inlogregistratiesysteem</h5>
-                        <p class="projectcard-text">In dit project laten we zien dat een student een inlogregistratiesysteem heeft leren maken</p>
-                        <a href="https://inlogregistratiesysteem-tarikzarouali600.000webhostapp.com/"
-                            class="btn btn-primary">inlogregistratiesysteem
-                            </a>
+                    <div class="projectrow">
+                        <div class="projectcard">
+                            <img src="" class="projectcard-img-top" alt="...">
+                            <div class="projectcard-body">
+                                <h5 class="projectcard-title">De inlogregistratiesysteem</h5>
+                                <p class="projectcard-text">In dit project laten we zien dat een student een
+                                    inlogregistratiesysteem heeft leren maken</p>
+                                <a href="https://inlogregistratiesysteem-tarikzarouali600.000webhostapp.com/"
+                                    class="btn btn-primary">inlogregistratiesysteem
+                                </a>
+                            </div>
+                        </div>
                     </div>
+
+
+
                 </div>
-            </div>
-
-            <div class="col-4">
-                <div class="projectcard">
-                    <img src="img/img1.PNG" class="projectcard-img-top" alt="...">
-                    <div class="projectcard-body">
-                        <h5 class="projectcard-title">De inlogregistratiesysteem</h5>
-                        <p class="projectcard-text">In dit project laten we zien dat een student een inlogregistratiesysteem heeft leren maken</p>
-                        <a href="https://inlogregistratiesysteem-tarikzarouali600.000webhostapp.com/"
-                            class="btn btn-primary">inlogregistratiesysteem
-                            </a>
-                    </div>
-                </div>
-            </div>
-
-
-</div>
 
 
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                    crossorigin="anonymous"></script>
 
-</section>
-  </body>
+    </section>
+</body>
+
 </html>
